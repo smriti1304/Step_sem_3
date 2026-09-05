129c1e721e8beb79e0cf6d8d431e28ebd7b096c3
Smriti Kumari <smritikumari13042007@gmail.com>
2026-09-05 20:03:39 +0530

    implimenting week 4

---
+++ Rotate.java
@@ -0,0 +1,30 @@
+public class Rotate {
+
+    static int[] rotateArray(int[] nums, int k) {
+
+        k = k % nums.length;
+
+        int[] newArray = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int newPosition = (i + k) % nums.length;
+
+            newArray[newPosition] = nums[i];
+        }
+
+        return newArray;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+
+        int[] result = rotateArray(nums, k);
+
+        for (int x : result) {
+            System.out.print(x + " ");
+        }
+    }
+}
