28da8e6b4b35c391f3f87eaa24df6534fdc0ea43
Smriti Kumari <smritikumari13042007@gmail.com>
2026-09-05 19:30:12 +0530

    Add Week 4 assignment

---
+++ Subarray.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Subarray {
+
+    static int subarraySum(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+
+        int currentSum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+
+            currentSum += num;
+
+            int required = currentSum - k;
+
+            if (map.containsKey(required)) {
+                count += map.get(required);
+            }
+
+            map.put(
+                currentSum,
+                map.getOrDefault(currentSum, 0) + 1
+            );
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 1, 1};
+        int k = 2;
+
+        System.out.println(subarraySum(nums, k));
+    }
+}
