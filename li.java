b59b3ed924c6253b3bf36aab117f626fd6774f97
Smriti Kumari <smritikumari13042007@gmail.com>
2026-09-07 11:06:38 +0530

    implemting week 3

---
+++ li.java
@@ -0,0 +1,16 @@
+class LibraryCard {
+    String holderName;
+    int booksIssued;
+    boolean active;
+}
+
+public class li {
+    public static void main(String[] args) {
+
+        LibraryCard card = new LibraryCard();
+
+        System.out.println("Holder: " + card.holderName);
+        System.out.println("Books Issued: " + card.booksIssued);
+        System.out.println("Active: " + card.active);
+    }
+}
