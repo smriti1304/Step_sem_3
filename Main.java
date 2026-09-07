8789d70ab99dbdbfd3df92b636e7e70548a002bc
Smriti Kumari <smritikumari13042007@gmail.com>
2026-09-07 11:09:50 +0530

    week2

--- Main.java
+++ Main.java
@@ -1,30 +1,22 @@
+class HostelRoom {
+    String roomNo;
+    int occupied;
+}
+
 public class Main {
-
-    static int maxProfit(int[] prices) {
-
-        int minPrice = prices[0];
-        int maxProfit = 0;
-
-        for (int i = 1; i < prices.length; i++) {
-
-            if (prices[i] < minPrice) {
-                minPrice = prices[i];
-            }
-
-            int profit = prices[i] - minPrice;
-
-            if (profit > maxProfit) {
-                maxProfit = profit;
-            }
-        }
-
-        return maxProfit;
-    }
-
     public static void main(String[] args) {
 
-        int[] prices = {7, 1, 5, 3, 6, 4};
+        HostelRoom roomA = new HostelRoom();
+        HostelRoom roomB = new HostelRoom();
 
-        System.out.println(maxProfit(prices));
+        roomA.roomNo = "C-101";
+        roomB.roomNo = "C-102";
+
+        roomA.occupied++;
+        roomA.occupied++;
+        roomA.occupied++;
+
+        System.out.println(roomA.roomNo + " occupied: " + roomA.occupied);
+        System.out.println(roomB.roomNo + " occupied: " + roomB.occupied);
     }
 }
