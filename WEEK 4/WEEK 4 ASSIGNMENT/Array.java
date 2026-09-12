a9abffa8e08324c354821782f9b88e96382c05f7
Smriti Kumari <smritikumari13042007@gmail.com>
2026-09-12 21:13:45 +0530

    Add files via upload

---
+++ WEEK 4/WEEK 4 ASSIGNMENT/Array.java
@@ -0,0 +1,36 @@
+public class Array {
+
+    static int[] productExceptSelf(int[] nums) {
+
+        int[] answer = new int[nums.length];
+
+        // Left products
+        int product = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            answer[i] = product;
+            product = product * nums[i];
+        }
+
+        // Right products
+        product = 1;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            answer[i] = answer[i] * product;
+            product = product * nums[i];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4};
+
+        int[] result = productExceptSelf(nums);
+
+        for (int x : result) {
+            System.out.print(x + " ");
+        }
+    }
+}
